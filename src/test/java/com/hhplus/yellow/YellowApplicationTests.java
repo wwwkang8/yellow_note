33c9004c0fda31290cd51aa9b27774cfd46968d0
wwwkang8 <wwwkang8@gmail.com>
2024-07-03 21:32:16 +0900

    initial commit

---
+++ src/test/java/com/hhplus/yellow/YellowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhplus.yellow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YellowApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
